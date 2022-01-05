bc3b0c4e0d6ae334246fd16a22c7ed86fbb1b1de
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2022-01-05 19:27:51 +0700

    ✨ Add post command

---
+++ cmd/post.go
@@ -0,0 +1,83 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+// postCmd represents the post command
+var postCmd = &cobra.Command{
+	Use:   "post",
+	Short: "Test post method",
+	Long:  `Send POST request to server with headers, params`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client := http.Client{}
+
+		url := ""
+		if len(args) > 0 && args[0] != "" {
+			url = args[0]
+		} else {
+			fmt.Printf("Url not found")
+			return
+		}
+
+		req, err := http.NewRequest("POST", url, nil)
+		if err != nil {
+			fmt.Printf("Url not valid: %v", err)
+			return
+		}
+
+		headers, err := cmd.Flags().GetStringSlice("header")
+		if err != nil {
+			fmt.Printf("Header not found: %v", err)
+			return
+		}
+
+		if len(headers) > 0 {
+			for _, header := range headers {
+				keyValue := strings.Split(header, ":")
+				key := strings.Trim(keyValue[0], " ")
+				value := strings.Trim(keyValue[1], " ")
+
+				req.Header.Set(key, value)
+			}
+		}
+
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Printf("Can not send request: %v", err)
+			return
+		}
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			fmt.Printf("Can not read response: %v", err)
+			return
+		}
+
+		result := string(body)
+		fmt.Printf(result)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(postCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// postCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// postCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
